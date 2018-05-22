51abfaa75d2afc62655077cd64be486edc0a5693
Frank Staude <frank@staude.net>
2018-05-22 17:54:27 +0200

    template

--- template.php
+++ template.php
@@ -1,5 +1,19 @@
-<html>
-<body>
+<?php
+$privacypageid = get_option( 'wp_page_for_privacy_policy' );
+$post = get_post( $privacypageid );
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php $post->post_title; ?></title>
+
+</head>
+<body class="docs ">
+
+
 
 <?php dynamic_sidebar( 'sidebar-dsgvo-top' ); ?>
 <?php
